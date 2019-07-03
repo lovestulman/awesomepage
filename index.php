1f43d6b4f2a9cbe555ea02e05a595d328fe78a22
Adam Mrozek <dat14amr@student.lu.se>
2019-07-03 14:11:46 +0200

    changed some lines

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	echo phpinfo();
+	if (isset($_POST['password'] === "")) {
+		echo "Access granted.";
+	}
+?>
+
+<!-- This is totally not a flag: CTF{totall5_n0t_4_Fl4g} -->
+<html>
+	<html>
+	</html>
+
+	<body background="#ubuntu">
+	<form name="login">
+		<input name="password" type="password" />
+	</body>
+
+</html>
+
